1a6a321f8f4134ed5fa789093a3ef70016e76479
yleizarg <82826080+yleizarg@users.noreply.github.com>
2024-07-10 14:41:54 +0200

    Add files via upload

---
+++ idade.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+def verificar_idade():
+    try:
+        ano_atual = datetime.now().year
+        ano_nascimento = int(input("Por favor, insira o seu ano de nascimento: "))
+        idade = ano_atual - ano_nascimento
+
+        if idade >= 18:
+            print(f"Você tem {idade} anos. Você é maior de idade.")
+        else:
+            print(f"Você tem {idade} anos. Você é menor de idade.")
+    except ValueError:
+        print("Por favor, insira um ano válido.")
+
+if __name__ == "__main__":
+    verificar_idade()
